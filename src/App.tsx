12a6fb24da53a9a3bb030f4ece61a0c527308f8b
OdagakiHiroki <email@example.com>
2021-11-08 22:56:12 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Top } from "components/pages/Top";
+import './App.css';
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Top />} />
+      </Routes>
+    </BrowserRouter>
+  );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+}
---
+++ src/components/pages/Top.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from "moment";
+import 'react-big-calendar/lib/sass/styles.scss';
+// import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss';
+
+const localizer = momentLocalizer(moment);
+
+export const Top: FC = () => {
+  return (
+    <>
+      <div>top</div>
+      <div>
+        <Calendar
+          localizer={localizer}
+          events={[]}
+          startAccessor="start"
+          endAccessor="end"
+          style={{height: "600px"}}
+        />
+      </div>
+    </>
+  )
+}
