de403f2c915e3d52a1e61eb8e48b64de164f6980
OdagakiHiroki <email@example.com>
2021-11-08 23:43:15 +0900

    add event hook

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -4,17 +4,20 @@ import moment from "moment";
 import "moment/locale/ja";
 import 'react-big-calendar/lib/sass/styles.scss';
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss';
+import { useGetEvents } from "hooks";
 
 const localizer = momentLocalizer(moment);
 
 export const Top: FC = () => {
+  const events = useGetEvents();
+
   return (
     <>
       <div>top</div>
       <div>
         <Calendar
           localizer={localizer}
-          events={[]}
+          events={events}
           startAccessor="start"
           endAccessor="end"
           style={{height: "600px"}}
