9d06998202f2d2362e648fc9b0999b442b9d6837
OdagakiHiroki <email@example.com>
2021-11-10 00:04:39 +0900

    add custom component, add current date state

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,28 +1,39 @@
-import { FC } from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+import { FC, useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "moment/locale/ja";
-import 'react-big-calendar/lib/sass/styles.scss';
-import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss';
+import "react-big-calendar/lib/sass/styles.scss";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.scss";
 import { useGetEvents } from "hooks";
 
 const localizer = momentLocalizer(moment);
 
+const Header = (date: string) => {
+  return <div>{date}</div>;
+};
+
 export const Top: FC = () => {
   const events = useGetEvents();
+  console.debug(localizer);
+  const [currentDate, setCurrentDate] = useState(new Date());
 
   return (
-    <>
-      <div>top</div>
-      <div>
-        <Calendar
-          localizer={localizer}
-          events={events}
-          startAccessor="start"
-          endAccessor="end"
-          style={{height: "600px"}}
-        />
-      </div>
-    </>
-  )
-}
+    <div>
+      <Calendar
+        localizer={localizer}
+        defaultView="day"
+        defaultDate={currentDate}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "600px" }}
+        // toolbar={false}
+        components={{
+          timeGutterHeader: () =>
+            Header(moment(currentDate).format("YYYYMMDD")),
+        }}
+        onNavigate={(date) => setCurrentDate(date)}
+      />
+    </div>
+  );
+};
