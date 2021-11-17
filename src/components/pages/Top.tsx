0b210f654690f426f17d4daab4c7a7e638f33c83
OdagakiHiroki <email@example.com>
2021-11-18 04:33:43 +0900

    add prev and next

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react'
 import FullCalendar, { CalendarApi } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import { formatDate } from "utils";
+import { Header } from "components/organisms/Header";
 import { useGetEvents } from "hooks";
 
 export const Top: React.VFC = () => {
@@ -8,15 +10,24 @@ export const Top: React.VFC = () => {
 
   const calendarRef = useRef<FullCalendar>(null);
   const [calendarApi, setCalendarApi] = useState<CalendarApi>();
+  const [currentDate, setCurrentDate] = useState(new Date());
 
   useEffect(() => {
     if (!calendarRef) {
       return;
     }
-    setCalendarApi(calendarRef?.current?.getApi());
+    const calendarApi = calendarRef?.current?.getApi();
+    setCalendarApi(calendarApi);
+    if (!calendarApi) {
+      return;
+    }
+    setCurrentDate(calendarApi?.getDate());
   }, [calendarApi])
 
   const addEvent = () => {
+    if (!calendarApi) {
+      return;
+    }
     calendarApi?.addEvent({
       title: "event",
       start: "2021-11-10",
@@ -24,8 +35,30 @@ export const Top: React.VFC = () => {
     });
   }
 
+  const prevClick = () => {
+    if (!calendarApi) {
+      return;
+    }
+    calendarApi?.prev();
+    setCurrentDate(calendarApi?.getDate());
+  }
+
+  const nextClick = () => {
+    if (!calendarApi) {
+      return;
+    }
+    calendarApi?.next();
+    setCurrentDate(calendarApi?.getDate());
+  }
+
   return (
     <div>
+      <Header
+        title={formatDate(currentDate, "yyyy年MM月dd日")}
+        // handleClickMenu={menuClick}
+        handleClickPrev={prevClick}
+        handleClickNext={nextClick}
+      />
       <div onClick={addEvent}>header</div>
       <FullCalendar
         ref={calendarRef}
