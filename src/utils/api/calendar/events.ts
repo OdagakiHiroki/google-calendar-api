e130945c6ef2f5eb3bb75fe0f4822336711c2ad4
OdagakiHiroki <email@example.com>
2021-11-23 12:31:35 +0900

    add get event list

---
+++ src/utils/api/calendar/events.ts
@@ -0,0 +1,29 @@
+import { thenReq } from "utils/gapi";
+import { getIsSignedIn } from "utils/api/auth/auth";
+
+type GetEventList = ({ calendarId }: { calendarId: string }) => Promise<{
+  nextSyncToken: string;
+  nextPageToken: string;
+  eventList: any[];
+}>;
+
+export const getEventList: GetEventList = async ({ calendarId }) => {
+  try {
+    const isSignedIn = await getIsSignedIn();
+    if (!isSignedIn) {
+      return { nextSyncToken: null, nextPageToken: null, eventList: [] };
+    }
+    const req = window.gapi.client.calendar.events.list({
+      calendarId
+    });
+    const { result, status } = await thenReq(req);
+    console.debug(await thenReq(req));
+    if (status !== 200) {
+      return { nextSyncToken: null, nextPageToken: null, eventList: [] };
+    }
+    const { nextSyncToken, nextPageToken, items } = result;
+    return { nextSyncToken, nextPageToken, eventList: items };;
+  } catch (error) {
+    return { nextSyncToken: null, nextPageToken: null, eventList: [] };
+  }
+};
